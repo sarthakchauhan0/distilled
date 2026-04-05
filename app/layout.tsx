d1b35935a357c4fbb3be64a5be1ad57bdc01d68b
SARTHAK CHAUHAN <sarthakchauhan@arizona.edu>
2026-04-05 20:58:40 +0530

    Favicon Updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,17 @@ export const metadata: Metadata = {
     "Software Application"
   ],
   authors: [{ name: "Starky Labs", url: "https://distilled.starkylabs.com" }],
+  icons: {
+    icon: [
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon.ico", sizes: "any" },
+    ],
+    apple: [
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+    ],
+  },
+  manifest: "/site.webmanifest",
   openGraph: {
     type: "website",
     locale: "en_US",
